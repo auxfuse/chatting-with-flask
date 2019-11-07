e495ba729b217883707b63ac91ebba72005c001f
auxfuse <aux.fuse@yahoo.com>
2019-11-07 23:21:15 +0000

    Storing messages in key-value pairs in dict

--- app.py
+++ app.py
@@ -1,28 +1,33 @@
 import os
 from datetime import datetime
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request, session
 
 app = Flask(__name__)
+app.secret_key = "randomthingystring123"
 messages = []
 
 def add_messages(username, message):
     """Add messages to the `messages` list on line 5"""
     now = datetime.now().strftime("%H:%M:%S")
-    messages.append(f"({now}) {username}: {message}")
+    messages_dict = {"timestamp": now, "from": username, "message": message}
+    messages.append(messages_dict)
 
-def get_all_messages():
-    """Get all messages and seperate using a `<br>`"""
-    return "<br>".join(messages)
-
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
     """Main page with instructions!"""
+
+    if request.method == "POST":
+        session["username"] = request.form["username"]
+
+    if "username" in session:
+        return redirect(session["username"])
+
     return render_template("index.html")
 
 @app.route('/<username>')
 def user(username):
     """Display Chat message"""
-    return f"<h1>Welcome, {username}</h1>{get_all_messages()}"
+    return f"<h1>Welcome, {username}</h1>{messages}"
 
 @app.route('/<username>/<message>')
 def send_message(username, message):
